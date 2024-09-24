cea236162f5c1d926667d51d3b86eac105f15347
measmarady <160995435+measmarady@users.noreply.github.com>
2024-09-24 10:18:02 +0700

    Add files via upload

---
+++ foldername/script.js
@@ -0,0 +1,20 @@
+const question = document.querySelector(".question")
+const gif = document.querySelector(".gif")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+
+yesBtn.addEventListener("click", ()=> {
+    question.innerHTML = "You are officially mine, Babe<3";
+    gif.src="https://media.tenor.com/0o_Jr21uSnwAAAAi/tonton-friends-tsundere-cat.gif"
+})
+noBtn.addEventListener("mouseover", ()=> {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX)
+    const randomY = Math.floor(Math.random() * maxY)
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px"
+})
